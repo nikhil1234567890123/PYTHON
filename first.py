7dcc2b481a479fe35969ec9683c99c19e2e99d3b
Nikhil Kaundal <120103802+nikhil1234567890123@users.noreply.github.com>
2025-01-09 18:13:35 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,92 @@
+# print("hello world")
+# print("nikhil")
+# name = "nikhil"
+# age = 20
+# price =25.99
+
+# age2 = age
+# print(age2)
+# print("My name is :", name)
+# print("My age is :", age)
+# print("My price is:", price)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+'''
+old = False
+a = None
+print(type(old))
+print(type(a))
+'''
+#arithmetic operators
+'''a = 2
+b = 4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)#modular operator (remainder)
+print(a**b)# a^b calculate'''
+
+#relational orperators
+'''a = 50
+b = 30
+
+print(a==b) #False
+print(a!=b)#True (not equal to )
+print(a>=b)#True
+print(a>b)#True
+print(a<=b)#False
+print(a<b)#False'''
+
+#assignment operators
+'num=10'
+'num=num + 10'
+'''num += 10 
+num -= 10
+num /= 10
+num **= 10
+print("num:", num)'''
+
+#Logical operators(not, and , or)
+'''print(not False)#True
+print(not True)#False
+a = 50
+b = 20
+print(not (a>b))#False
+
+val1 = True
+val2 = True
+val3 =False
+'print("AND operator:", val1 and val2)#true'
+print("AND operator:", val1 and val3)#False
+print("OR operator:", val1 or val2)#true
+print("OR operator:", val1 or val3)#true
+print("OR operator:", (a==b) or (a>b))#True'''
+
+#Type conversion
+'''a = 2
+b =4.25
+
+sum = a+b #2.0 + 4.25 =>6.25'''
+
+#Type casting
+'''a = int(2)
+b = 4.25
+
+sum = a+b
+print(sum)'''
+
+#input in python
+'''name = input("enter your name:")
+print("Welcome", name)'''
+'''age = float(input("enter your age:"))
+print("your age is:", type(age),age)'''
+
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+marks = float(input("Enter your marks:"))
+
+print("welcome", name)
+print("your age is ", age)
+print("your obtained marks are", marks)
