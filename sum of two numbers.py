8ec63079d1e09aad944b01dcb42919d5cc2aaf4a
Nikhil Kaundal <120103802+nikhil1234567890123@users.noreply.github.com>
2025-01-12 15:06:14 +0530

    Add files via upload

---
+++ sum of two numbers.py
@@ -0,0 +1,18 @@
+#add two munbers
+'''a = int(input("enter your first number:"))
+b = int(input("Enter your second number:"))
+print("sum of a and b is ", a + b)'''
+
+#WAP ti input side of a square and print its area 
+'''side = float(input("Enter side of a square:"))
+print("area of square is ", side*side)'''
+
+#WAP to input 2 floating values point numbers and print there avarage 
+'''a = float(input("enter first value:"))
+b = float(input("enter second value:"))
+print("the average is ", (a+b)/2)'''
+
+#WAP to input 2 int numbers, a and b. print true if a is grerater then or equal to b. if not print false
+'''a = int(input("enter first number:"))
+b = int(input("enetr second number:"))
+print(a>=b)'''
